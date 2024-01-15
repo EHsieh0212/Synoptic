004ffd00f158462611f5a8124c8abfb6c48dad13
Elaine Hsieh <elaine.hsieh.int@shopback.com>
2024-01-15 23:59:05 +0800

    fix: add cart router

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const productRouter = require("./productRouter");
 const userRouter = require("./userRouter");
+const cartRouter = require("./cartRouter")
 const orderRouter = require("./orderRouter");
 const router = express.Router();
 
 router.use("/products", productRouter);
+router.use("/cart", cartRouter);
 router.use("/users", userRouter);
 router.use("/orders", orderRouter);
 
