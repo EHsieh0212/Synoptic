8b80191daf723c8a64cfd808e307c6f41867b3a3
Elaine Hsieh <elaine.hsieh.int@shopback.com>
2024-01-23 16:08:02 +0800

    add: thankYou page

--- client/src/Components/Thanks/index.js
+++ client/src/Components/Thanks/index.js
@@ -1,13 +1,34 @@
 import styled from "styled-components";
+import check from '../../Assests/check.png'
+
+
+const StyledContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding-top: 100px;
+    padding-bottom: 200px;
+`;
+
 
 const StyledThanks = styled.div`
+    font-size: 80px;
+`;
 
+const StyledComment = styled.div`
+    padding-top: 50px;
+    font-size: 17px;
 `;
 
 const Thanks = () => {
 
-    return(
-        <StyledThanks>thanks</StyledThanks>
+    return (
+        <StyledContainer>
+            <StyledThanks>Thank You!</StyledThanks>
+            <img src={check} alt="check" />
+            <StyledComment> Your Order Is Completed Successfully. Thank You For Shopping At Synoptic. </StyledComment>
+        </StyledContainer>
     )
 };
 
